574abbf91b2cff5d2a684abd0217850f9828e673
kimts2628 <tsts_@likelion.net>
2021-05-16 04:49:56 +0900

    Complete refactoring Python -> Swift 1 task

--- swift_refactor.swift
+++ swift_refactor.swift
@@ -1,1 +1,11 @@
-print("hello world")
+func multipleAorB(maxNum: Int, a: Int, b: Int) -> Int {
+    var result = 0;
+    for num in a...maxNum - 1 {
+        if num % a == 0 || num % b == 0 {
+            result += num;
+                    print(result);
+        }
+    }
+    return result;
+}
+print(multipleAorB(maxNum: 1000, a: 3, b: 5))
